b2bc1b26173cdfbe236b2be3f49c5b849794dc84
Shakil57375 <shakil57375@gmail.com>
2023-04-30 19:33:25 +0600

    vercel file added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 const catagories = require('./data/catagories.json')
-
+const news = require('./data/news.json')
 app.use(cors());
 app.get('/', (req, res)=>{
     res.send("Dragon is running")
@@ -11,6 +11,28 @@ app.get('/', (req, res)=>{
 
 app.get('/catagories', (req, res)=>{
     res.send(catagories);
+})
+
+app.get('/news', (req, res)=>{
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id
+    console.log(id);
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
+})
+
+app.get('/catagories/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, ()=>{
